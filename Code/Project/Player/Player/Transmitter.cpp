e74470fda08de9c392ea3c709cfb1ebd4a84f315
StjerneIdioten <dragonest7@gmail.com>
2015-10-30 17:38:28 +0100

    Created a working generator class. That can take an array of DTMF tones and genereate a sample array with them all. With then can be played.

---
+++ Code/Project/Player/Player/Transmitter.cpp
@@ -0,0 +1,17 @@
+#include "Transmitter.h"
+
+namespace DTMF {
+	Transmitter::Transmitter()
+	{
+	}
+
+
+	Transmitter::~Transmitter()
+	{
+	}
+
+	void Transmitter::Transmit(std::vector<int>) {
+
+	}
+
+}
