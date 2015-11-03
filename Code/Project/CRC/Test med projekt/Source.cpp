83a2eb723f25990fe29eff443ba72ab2beb069bc
Javakin <danielharaldson@yahoo.com>
2015-11-03 13:26:41 +0100

    Tilføjedt alle #include (reverted from commit a7c42d8de31d3f66f8ad4230be1f98a0978d65ac)

--- Code/Project/CRC/Test med projekt/Source.cpp
+++ Code/Project/CRC/Test med projekt/Source.cpp
@@ -9,7 +9,6 @@ using namespace std;
 const int GENERATOR = 0x107 << 7;
 
 //function declrations
-void bitStuffing(vector<int>&, vector<int>&);
 
 void generateCRC(vector<int>&);
 //adds a CRC byte in the end of vector 
@@ -112,52 +111,3 @@ void generateCRC(vector<int>& iVector)
 	iVector.push_back(iByte);
 }
 
-void bitStuffing(vector<int>& iVector, vector<int>& stuffVector)
-{
-	//opsætning af variable
-	stuffVector.clear();
-	int iByte = 0;
-	int Loop = 0;
-
-	int stuffByte = 0;
-	int stuffLoop = 0;
-
-
-
-	//Syndromet beregnes
-	for (int i : iVector)
-	{
-		iByte = iByte | i;
-		Loop = 0;
-		while (Loop < 8)
-		{
-			bitset<16> s(iByte);
-			if (!s[15])
-			{
-				cout << s << endl;
-				if (s[14] && s[13] && s[12] && s[11] && s[10])
-				{
-					cout << "dette er et falg der tilføjes " << 0 << endl;
-					//to do
-				}
-			}
-			else
-			{
-				stuffByte = stuffByte >> 1 | 1;
-				stuffLoop++;
-			}
-
-
-			if (!(stuffLoop < 8))
-			{
-
-			}
-			iByte = iByte << 1;
-			Loop++;
-		}
-	}
-
-	//Syndromet returneres
-	cout << "Syndromet er: " << iByte << endl;
-	iVector.pop_back();
-}
