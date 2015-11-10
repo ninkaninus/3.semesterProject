1a9adc6c113abbea5e0b6a3c4ff63046ce6363c2
Javakin <danielharaldson@yahoo.com>
2015-11-10 18:34:47 +0100

    CRC check på både modtager siden og afsender siden understøtter nu bool vektore Understøtter CRC8, CRC10, CRC16 og CRC32, samt en defoult ved CRC8

--- Code/Project/CRC/Test med projekt/Source.cpp
+++ Code/Project/CRC/Test med projekt/Source.cpp
@@ -5,109 +5,249 @@
 
 using namespace std;
 
-//definitions
-const int GENERATOR = 0x107 << 7;
 
 //function declrations
-
-void generateCRC(vector<int>&);
+void generateCRC(vector<bool>&, int& n);
 //adds a CRC byte in the end of vector 
 
-void ChekCRC(vector<int>&);
+bool ChekCRC(vector<bool>&, int& n);
 //checks CRC byte in the end of vector 
+
+//hjælpefunktioner
+void print(vector<bool>& aVector, string aName);
+void fyldVector(vector<bool>& aVector);
+void tilføjFejl(vector<bool>& aVector);
 
 
 int main() {
-	cout << 1 << endl;
-		vector<int> hans;
-		vector<int> stuff;
-		stuff.push_back(0x00);
 		
-		cout << 2 << endl;
-		hans.push_back(0x1F);
-		hans.push_back(0xFF);
-		hans.push_back(0x87);
-		hans.push_back(0x79);
-		hans.push_back(0x10);
-		hans.push_back(0xFF);
-		hans.push_back(0x87);
-		hans.push_back(0x79);
-		hans.push_back(0xFA);
-		hans.push_back(0x11);
-		hans.push_back(0x00);
+		//opsætning
+		vector<bool> hans;
+		fyldVector(hans);
+		print(hans, "hans original");	
+		
+		//afsender siden
+		int n = 32;
+		generateCRC(hans, n);
+		print(hans, "hans med CRC");
 
-		cout << 3 << endl;
-			//afsender del
-	
-		//bitStuffing(hans, stuff);
+		//fejl tilføjes
+		//tilføjFejl(hans);
+		//print(hans, "hans med fejl");
+		
 
-		for (unsigned int y=0; y < hans.size(); y++) {
-			cout << hans[y] << endl;
-		}
-		cout << 4 << endl;
-		hans.clear();
 
-		for (unsigned int y = 0; y < hans.size(); y++) {
-			cout << hans[y] << endl;
-		}
+		//modtager siden
+		cout << "dette udskriver crc status: " << ChekCRC(hans, n) << endl;
+		print(hans, "hans original");
 
 		return 0;
 }
 
 	//function definitions		
-void ChekCRC(vector<int>& iVector) {
-	//opsætning af variable
-	int iByte = 0;
-	int Loop = 0;
-
-	//Syndromet beregnes
-	for (int i : iVector)
-	{
-		iByte = iByte | i;
-		Loop = 0;
-		while (Loop < 8)
-		{
-			bitset<16> s(iByte);
-			if (s[15])
-				iByte = iByte ^ GENERATOR;
-
-			iByte = iByte << 1;
-			Loop++;
-		}
-	}
-
-	//Syndromet returneres
-	cout << "Syndromet er: " << iByte << endl;
-	iVector.pop_back();
-	
-}
-
-void generateCRC(vector<int>& iVector)
+void generateCRC(vector<bool>& bVector, int& n)
 {
+	//Valg af generatorpolynomium
+	double GENERATOR = 0;
+	
+	switch (n)
+	{
+	case 8:
+		GENERATOR = 0b100000111;
+		break;
+	case 10:
+		GENERATOR = 0b11000110101;
+		break;
+	case 16:
+		GENERATOR = 0b10001000000100001;
+		break;
+	case 32:
+		GENERATOR = 0b100000100110000010001110110110111;
+		break;
+	default:
+		cout << "Fejl CRC ugyldigt skal vaere 8, 10, 16 eller 32 " << endl << n << " blev brugt, der erstattes med " << 8 << endl;
+		GENERATOR = 0b100000111;
+		n = 8;
+	}
+
+
+	bitset<33> G(GENERATOR);
+
 	//opsætning
-	iVector.push_back(0x00);
-	int iByte = 0;
-	int Loop = 0;
+	vector<bool> CRC;
+	for (bool i : bVector)
+		CRC.push_back(i);
+	for (int i = 0; i < n; i++)
+		CRC.push_back(0);
+		
 	
 	//Syndromet beregnes
-	for (int i : iVector)
+	for (unsigned int i = 0; i < CRC.size() - n; i++)
 	{
-		iByte = iByte | i;
-		Loop = 0;
-		while (Loop < 8)
-		{
-			bitset<16> s(iByte);
-			if (s[15])
-				iByte = iByte ^ GENERATOR;
+		if (CRC[i]) {
+			// der er 1 - XOR
+			for (int GEN = 0; GEN < n + 1; GEN++)
+			{
+				CRC[i + GEN] = CRC[i + GEN] ^ G[n - GEN];
+			}
+		}
+	}
+		
+	//Syndrom tilføjes
+	cout << "CRC er: ";
+	for (unsigned int i = CRC.size() - n; i < CRC.size();i++) {
+		bVector.push_back(CRC[i]);
+		cout << CRC[i];
+	}
+}
 
-			iByte = iByte << 1;
-			Loop++;
+bool ChekCRC(vector<bool>& bVector, int& n) {
+	//Valg af generatorpolynomium
+	double GENERATOR = 0;
+	
+	switch (n)
+	{
+	case 8:
+		GENERATOR = 0b100000111;
+		break;
+	case 10:
+		GENERATOR = 0b11000110101;
+		break;
+	case 16:
+		GENERATOR = 0b10001000000100001;
+		break;
+	case 32:
+		GENERATOR = 0b100000100110000010001110110110111;
+		break;
+	default:
+		cout << "Fejl CRC ugyldigt skal vaere 8, 10, 16 eller 32 " << endl << n << " blev brugt, der erstattes med " << 8 << endl;
+		GENERATOR = 0b100000111;
+		n = 8;
+	}
+
+	bitset<33> G(GENERATOR);
+	
+
+	//Opsætning
+	vector<bool> CRC;
+	for (bool i : bVector)
+		CRC.push_back(i);
+
+
+	//Syndromet beregnes
+	for (unsigned int i = 0; i < CRC.size() - n; i++)
+	{
+		if (CRC[i]) {
+			// der er 1 - XOR
+			for (int GEN = 0; GEN < n + 1; GEN++)
+			{
+				CRC[i + GEN] = CRC[i + GEN] ^ G[n - GEN];
+			}
 		}
 	}
 
-	//Syndrom tilføjes
-	iByte = iByte >> 8;
-	iVector.pop_back();
-	iVector.push_back(iByte);
+
+	//Syndrom fjernes
+	for (unsigned int i = CRC.size() - n; i < CRC.size();i++) {
+		bVector.pop_back();
+	}
+
+	//returner 1 hvis syndromet er 0 (ingen fejl) ellers returner 0
+	for (bool i : CRC)
+	{
+		if (i)
+			return 0;
+	}
+	return 1;
 }
 
+
+//funktioner til debugging
+void print(vector<bool>& aVector, string aName)
+{
+	cout << endl << "dette er  " << aName << endl;
+	int space = 0;
+	for (int i : aVector)
+	{
+		cout << i;
+		space++;
+		if (space > 7)
+		{
+			space = 0;
+			cout << " ";
+		}
+	}
+	cout << endl << endl;
+}
+
+void fyldVector(vector<bool>& aVector)
+{
+	aVector.push_back(0);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(0);
+	aVector.push_back(1);
+	aVector.push_back(0);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(0);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(1);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(0);
+	aVector.push_back(1);
+	aVector.push_back(0);
+	aVector.push_back(1);
+
+
+}
+
+void tilføjFejl(vector<bool>& aVector) {
+	int index = 5;
+	int index2 = 6;
+	int index3 = 7;
+	vector<bool> temp;
+
+	for (unsigned int i = 0; i < aVector.size(); i++) {
+		if (i == index) {
+			temp.push_back(1);
+		}
+		else if (i == index2) {
+			temp.push_back(0);
+		}
+		else if (i == index3) {
+			temp.push_back(1);
+		}
+		else
+			temp.push_back(aVector[i]);
+	}
+	
+	aVector.clear();
+	for (bool i : temp) {
+		aVector.push_back(i);
+	}
+}
