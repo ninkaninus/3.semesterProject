4fc04bea44de25298d5772dfa7edf1cb95896286
Javakin <danielharaldson@yahoo.com>
2015-11-10 11:09:49 +0100

    Semi funktionel bitstuffing algoritme i karakter orienteret vektorstruktur

--- Code/Project/Bitset/Test med projekt/Source.cpp
+++ Code/Project/Bitset/Test med projekt/Source.cpp
@@ -5,112 +5,212 @@
 
 using namespace std;
 
-//definitions
-const int GENERATOR = 0x107 << 7;
-
 //function declrations
-void generateCRC(vector<int>&);
-//adds a CRC byte in the end of vector 
 
-void ChekCRC(vector<int>&);
-//checks CRC byte in the end of vector 
+//Afsender siden
+void stuffinator(vector<int>& stuffVector, bool bit);
+void copyStuffToPayload(vector<int>& iVector, vector<int>& stuffVector);
+void bitStuffing(vector<int>& iVector);
+//bitstuffing og 0 padding (note: der paddes ikke efter standarden)
+
+
+//modtager siden
+void antiBitStuffing(vector<int>& iVector);
+
+
+//hjælpefunktioner
+void print(vector<int>& aVector, string aName);
+void fyldVector(vector<int>& aVector);
 
 
 int main() {
+		//opsætning
 		vector<int> hans;
-		
-		hans.push_back(0x1F);
-		hans.push_back(0xFF);
-		hans.push_back(0x87);
-		hans.push_back(0x79);
-		hans.push_back(0x10);
-		hans.push_back(0xFF);
-		hans.push_back(0x87);
-		hans.push_back(0x79);
-		hans.push_back(0xFA);
-		hans.push_back(0x11);
-		hans.push_back(0x00);
+		fyldVector(hans);
 
+		//afsender siden bitstuffing
+		print(hans, "hans");
+		bitStuffing(hans);
+		print(hans, "hans med bitstuffing vertion 2.0");
+	
 
-		//afsender del
-		generateCRC(hans);	
-
-		for (int y=0; y < hans.size(); y++) {
-			cout << hans[y] << endl;
-		}
-		
-		cout << "der tilføjes fejl på plas 2" << endl << endl;
-
-		hans[2] = 0x88;
-		hans[10] = 0x73;
-
-		for (unsigned int y = 0; y < hans.size(); y++) {
-			cout << hans[y] << endl;
-		}
-		cout << endl;
-
-		//modtager
-		ChekCRC(hans);
-		for (unsigned int y = 0; y < hans.size(); y++) {
-			cout << hans[y] << endl;
-		}
+		//Modtagersiden siden bitstuffing
+		antiBitStuffing(hans);
+		print(hans, "hans original");
 
 		return 0;
 }
 
-	//function definitions		
-void ChekCRC(vector<int>& iVector) {
+
+//function definitions		
+void bitStuffing(vector<int>& iVector)
+{
 	//opsætning af variable
+	vector<int> stuffVector;
+	stuffVector.push_back(0x01);
+	iVector.push_back(0x00);
+	
 	int iByte = 0;
 	int Loop = 0;
+	int igen = 5;
 
-	//Syndromet beregnes
-	for (int i : iVector)
+	//Stuffing
+	for (unsigned int i = 0; i < iVector.size() - 1; i++)
 	{
-		iByte = iByte | i;
+		iByte = iByte | iVector[i];
 		Loop = 0;
 		while (Loop < 8)
 		{
 			bitset<16> s(iByte);
-			if (s[15])
-				iByte = iByte ^ GENERATOR;
-
+			stuffinator(stuffVector, s[7]);
+			if ((s[7] && s[8] && s[9] && s[10] && s[11] && s[12] && (!(s[13]))))
+			{	
+				stuffinator(stuffVector, 1);
+			}
 			iByte = iByte << 1;
 			Loop++;
 		}
 	}
 
-	//Syndromet returneres
-	cout << "Syndromet er: " << iByte << endl;
-	iVector.pop_back();
-	
+	//Afslut med at lægge alt tilbage i den gamle vektor
+	copyStuffToPayload(iVector, stuffVector);
 }
 
-void generateCRC(vector<int>& iVector)
+void stuffinator(vector<int>& stuffVector, bool bit)
+{
+	//opsætning af værider
+	int stuffByte = stuffVector[stuffVector.size() - 1];
+	bitset<8> b(stuffByte);
+	
+	//chekke om sidste byte er fyldt
+	if (b[7])
+	{
+		stuffByte = (stuffByte << 1) + bit;
+		stuffVector.pop_back();
+		stuffVector.push_back(stuffByte);
+
+		stuffVector.push_back(0x01);
+	}
+	else {
+		stuffByte = (stuffByte << 1) + bit;
+		stuffVector.pop_back();
+		stuffVector.push_back(stuffByte);
+	}
+
+
+
+	//display 
+	/*for (int i : stuffVector)	{
+		bitset<8> b(i);
+		cout << b << " ";
+	}
+	cout << endl;*/
+}
+
+void copyStuffToPayload(vector<int>& iVector, vector<int>& stuffVector) 
+{
+	iVector.clear();
+	for (int i : stuffVector) {
+		iVector.push_back(i);
+	}
+}
+
+
+void antiBitStuffing(vector<int>& iVector)
 {
 	//opsætning
+	vector<int> stuffVector;
+	stuffVector.push_back(0x01);
 	iVector.push_back(0x00);
-	int iByte = 0;
+
+	unsigned int iByte = 0;
 	int Loop = 0;
-	
-	//Syndromet beregnes
-	for (int i : iVector)
+	cout << "1." << endl;
+	//antistuffing
+	for (unsigned int i = 0; i < iVector.size() - 1; i++)
 	{
-		iByte = iByte | i;
+		iByte = iByte | iVector[i];
 		Loop = 0;
 		while (Loop < 8)
 		{
 			bitset<16> s(iByte);
-			if (s[15])
-				iByte = iByte ^ GENERATOR;
+			//cout << s << endl;
+			if (!(s[7] && s[8] && s[9] && s[10] && s[11] && s[12] && (!s[13]))) {
+				stuffinator(stuffVector, s[7]);
+			}
 
 			iByte = iByte << 1;
 			Loop++;
 		}
 	}
 
-	//Syndrom tilføjes
-	iByte = iByte >> 8;
-	iVector.pop_back();
-	iVector.push_back(iByte);
+	cout << "2." << iByte << endl;
+	//behandling af den sidst byte (lappeløsning)
+	int n = 0;
+	bitset<16> u(iByte);
+	cout << u << endl;
+	iByte = iByte & 0b1111111111111111;
+	bitset<16> s(iByte);
+	cout << s << endl;
+	while (iByte < 0b1000000000000000) {
+		iByte *= 2;
+		bitset<16> s(iByte);
+		cout << "nu:    " << s << endl;
+		n++;
+	}
+	cout << "3." << endl;
+
+	iByte = iByte >> 7;
+	iByte = iByte & 0b11111111;
+	iByte = iByte | (iVector[iVector.size() - 3] << 8);
+
+	n = 8 - n;
+
+	bitset<16> ta(iByte);
+	//cout << "efter: " << ta << endl;
+	//cout << n <<  endl;
+
+	while (n > 0)
+	{
+		bitset<16> s(iByte);
+
+		if (!(s[7] && s[8] && s[9] && s[10] && s[11] && s[12] && (!s[13]))) {
+			stuffinator(stuffVector, s[7]);
+		}
+
+		iByte = iByte << 1;
+		n--;
+	}
+
+	//smid resultatet tilbage i iVector
+	stuffVector.pop_back();
+	copyStuffToPayload(iVector, stuffVector);
+
+}
+
+//funktioner til debugging
+void print(vector<int>& aVector, string aName) 
+{
+	cout << endl << "dette er " << aName << endl;
+	for (int i : aVector)
+	{
+		bitset<8> b(i);
+		cout << b << " ";
+	}
+	cout << endl;
+}
+
+void fyldVector(vector<int>& aVector) 
+{
+	aVector.push_back(0x77);
+	aVector.push_back(0x00);
+	aVector.push_back(0xF7);
+	aVector.push_back(0x8B);
+	aVector.push_back(0xA8);
+	aVector.push_back(0x01);
+	aVector.push_back(0xFC);
+	aVector.push_back(0x80);
+	aVector.push_back(0x88);
+	aVector.push_back(0x01);
+
 }
