b2c78762010927ce3ab721b7de41c93c24dffb0c
StjerneIdioten <dragonest7@gmail.com>
2015-11-10 16:10:48 +0100

    Implemented a sort of soft transition. The volume gets louder untill it hits a peak in the middle of a town and then fades out. It works really well on the receiving end and the speakers don't pop.

--- Code/Project/Player/Player/Source.cpp
+++ Code/Project/Player/Player/Source.cpp
@@ -11,13 +11,13 @@ int main()
 
 	DTMF::Generator gen;
 
-	gen.setDuration(1.0/12.5);
+	gen.setDuration(1.0/10);
 
-	gen.setVolume(15000);
+	gen.setVolumeMax(15000);
 
-	gen.setTransitionMode(DTMF::Transition::zeroPadding);
+	//gen.setTransitionMode(DTMF::Transition::zeroPadding);
 
-	std::vector<DTMF::Tone> arr = { DTMF::Tone::ZERO, DTMF::Tone::ONE, DTMF::Tone::TWO, DTMF::Tone::THREE, DTMF::Tone::FOUR, DTMF::Tone::FIVE, DTMF::Tone::SIX, DTMF::Tone::SEVEN, DTMF::Tone::EIGHT, DTMF::Tone::NINE, DTMF::Tone::A, DTMF::Tone::B,  DTMF::Tone::C,  DTMF::Tone::D, DTMF::Tone::ASTERISK, DTMF::Tone::HASH };
+	std::vector<DTMF::Tone> arr = { DTMF::Tone::ASTERISK, DTMF::Tone::HASH, DTMF::Tone::ZERO, DTMF::Tone::ONE, DTMF::Tone::TWO, DTMF::Tone::THREE, DTMF::Tone::FOUR, DTMF::Tone::FIVE, DTMF::Tone::SIX, DTMF::Tone::SEVEN, DTMF::Tone::EIGHT, DTMF::Tone::NINE, DTMF::Tone::A, DTMF::Tone::B,  DTMF::Tone::C,  DTMF::Tone::D, DTMF::Tone::ZERO, DTMF::Tone::ZERO };
 	
 	sf::SoundBuffer* buff = gen.generate(arr);
 
