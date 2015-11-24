90f4ed7dd5325b1f296f5edf655793383186f084
Gugfann <kmolha@gmail.com>
2015-11-24 13:18:36 +0100

    Der kommer noget analyzer

---
+++ Code/Project/Analyzer/GoertzelTest/MyRecorder.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "SFML\Audio\SoundRecorder.hpp"
+#include "SFML\Audio\SoundBuffer.hpp"
+//#include "Analyzer.h"
+#include <iostream>
+#include <vector>
+
+class MyRecorder :
+	public sf::SoundRecorder
+{
+public:
+	MyRecorder();
+	MyRecorder(int aPInterval);
+	
+	virtual bool onStart();
+	virtual bool onProcessSamples(const sf::Int16* samples, std::size_t sampleCount);
+	virtual void onStop();
+
+	int getBufferSize();
+	std::vector<signed short> getBuffer();
+	std::vector<signed short> extractBuffer();
+	~MyRecorder();
+protected:
+	int processingInterval;
+	std::vector<signed short> buffer;
+};
+
