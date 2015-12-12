c0f42f1d19873ec8c6f924bd21d6eb3297f5d4ea
Javakin <danielharaldson@yahoo.com>
2015-12-12 15:16:37 +0100

    det samme

--- Code/Project/Main/Main/Main.cpp
+++ Code/Project/Main/Main/Main.cpp
@@ -16,42 +16,8 @@ using namespace std;
 int main()
 {
 	ApplicationLayer objA;
-	//DataLinkReceive objD;
-	//objD.init(8000, 25);
 
-	string hej;
-
-	PhysicalReceive Receiver;
-	Receiver.init(4000, 20);
-
-	Receiver.startRecording();
-	Sleep(5000);
-	Receiver.stopRecording();
-
-	Receiver.printMagnitudes();
-
-	//while(true)
-	//{	
-	//	//getline(cin, hej);
-	//	//objA.send(hej);
-	//	//objD.makeMessage();
-	//}
-
-
-	// bool to data
-	//cout << objA.BooleanTodata(objA.dataToBoolean(test)) << endl;
-
-	// Opsætningn af recieve (simulering af valid omdtaget data)
-	
-
-	
-	//
-	//objA.print(objD.getFrame(), "framen");
-
-	
-	// objA.print(objR.getCharInBool(),"print fra objR");
-	// objA.print(objR.getCharInBool(), "print fra objR");
-	
+	objA.send("Hej");
 
 	return 0;
 }
