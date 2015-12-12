ca9a61048cd893782ae15b0fcdf3811bac1fe0bf
Javakin <danielharaldson@yahoo.com>
2015-12-12 19:07:25 +0100

    Små ændringer

--- Code/Project/Main/Main/Main.cpp
+++ Code/Project/Main/Main/Main.cpp
@@ -13,65 +13,17 @@
 
 using namespace std;
 
-void compareFiles() {
-	ifstream Received("CharsReceived.txt");
-
-	string charsReceived;
-
-	while (Received.good())
-	{
-		charsReceived += Received.get();
-	}
-
-	ifstream Sent("CharsSent.txt");
-
-	string charsSent;
-
-	while (Sent.good())
-	{
-		charsSent += Sent.get();
-	}
-	
-	int lengthReceived = charsReceived.size();
-	int lengthSent = charsSent.size();
-
-	if (lengthReceived == lengthSent)
-	{
-		int errorCount = 0;
-
-		for (unsigned int i = 0; i < lengthReceived; i++)
-		{
-			if (charsReceived[i] != charsSent[i])
-				errorCount++;
-		}
-
-		double percentage = double(errorCount) / double(lengthReceived);
-
-		cout << "Found " << errorCount << " errors in " << lengthReceived << " characters" << endl;
-		cout << "Received differs from sent by: " << percentage << " %" << endl;
-	}
-	else
-	{
-		cout << "Sent " << lengthSent << " characters" << endl;
-		cout << "Received " << lengthReceived << " characters" << endl;
-	}
-		 
-}
-
 int main()
 {
-
 	ApplicationLayer objA;
+	DataLinkReceive objD;
 
-	string hej;
-
-	while (true)
-	{
-		getline(cin, hej);
-		objA.send(hej);
-		getline(cin, hej);
-		compareFiles();
+	//objA.send("Penis");
+	objD.init(8000,25);
+	while (true) {
+		objD.makeMessage();
 	}
+	
 
 	return 0;
 }
