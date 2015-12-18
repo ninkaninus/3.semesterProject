6341b8412ba42a64ab54404ca4e4d84788bdd073
Yubh8n <firestorm_sjoveren@hotmail.com>
2015-12-18 03:15:54 +0100

    123

--- Code/Project/DTMF User Interface/DTMF User Interface/GeneratedFiles/ui_gui.h
+++ Code/Project/DTMF User Interface/DTMF User Interface/GeneratedFiles/ui_gui.h
@@ -28,7 +28,7 @@ class Ui_GuiClass
 {
 public:
     QWidget *centralWidget;
-    QWidget *layoutWidget;
+    QVBoxLayout *verticalLayout_2;
     QVBoxLayout *verticalLayout;
     QPlainTextEdit *textVindue;
     QHBoxLayout *horizontalLayout;
@@ -39,21 +39,24 @@ public:
     {
         if (GuiClass->objectName().isEmpty())
             GuiClass->setObjectName(QStringLiteral("GuiClass"));
-        GuiClass->resize(1280, 600);
-        GuiClass->setMinimumSize(QSize(1280, 600));
+        GuiClass->resize(798, 400);
+        GuiClass->setMinimumSize(QSize(0, 0));
         GuiClass->setMaximumSize(QSize(1280, 600));
         centralWidget = new QWidget(GuiClass);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        layoutWidget = new QWidget(centralWidget);
-        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
-        layoutWidget->setGeometry(QRect(0, 0, 1281, 601));
-        verticalLayout = new QVBoxLayout(layoutWidget);
+        centralWidget->setEnabled(true);
+        centralWidget->setMaximumSize(QSize(800, 400));
+        verticalLayout_2 = new QVBoxLayout(centralWidget);
+        verticalLayout_2->setSpacing(0);
+        verticalLayout_2->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_2->setObjectName(QStringLiteral("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(0, 0, 0, 0);
+        verticalLayout = new QVBoxLayout();
         verticalLayout->setSpacing(0);
-        verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-        verticalLayout->setContentsMargins(0, 0, 0, 0);
-        textVindue = new QPlainTextEdit(layoutWidget);
+        textVindue = new QPlainTextEdit(centralWidget);
         textVindue->setObjectName(QStringLiteral("textVindue"));
+        textVindue->setEnabled(true);
         textVindue->setMinimumSize(QSize(0, 0));
         textVindue->setMaximumSize(QSize(16777215, 16777215));
         QFont font;
@@ -67,19 +70,23 @@ public:
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setSpacing(0);
         horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
-        textEdit = new QLineEdit(layoutWidget);
+        textEdit = new QLineEdit(centralWidget);
         textEdit->setObjectName(QStringLiteral("textEdit"));
+        textEdit->setEnabled(true);
         textEdit->setFont(font);
 
         horizontalLayout->addWidget(textEdit);
 
-        sendButton = new QPushButton(layoutWidget);
+        sendButton = new QPushButton(centralWidget);
         sendButton->setObjectName(QStringLiteral("sendButton"));
 
         horizontalLayout->addWidget(sendButton);
 
 
         verticalLayout->addLayout(horizontalLayout);
+
+
+        verticalLayout_2->addLayout(verticalLayout);
 
         GuiClass->setCentralWidget(centralWidget);
 
