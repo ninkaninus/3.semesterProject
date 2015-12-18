fc45e527159d8ca01e02ab17276877a786a55b75
William Bergmann Børresen <william@borresen.one>
2015-12-18 07:53:32 +0100

    it works now

--- Code/Project/Main/Main/Main.cpp
+++ Code/Project/Main/Main/Main.cpp
@@ -15,7 +15,6 @@ using namespace std;
 
 int main()
 {
-/*
 	ApplicationLayer objA;
 
 	string hej;
@@ -25,14 +24,6 @@ int main()
 		getline(cin, hej);
 		objA.send(hej);
 	}
-*/
-
-	PhysicalReceive receiver;
-	receiver.startRecording();
-	Sleep(5000);
-	receiver.stopRecording();
-
-	receiver.printMagnitudes();
 
 	return 0;
 }
